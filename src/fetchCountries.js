c1017efe7d00a94c84d240a71e422164333c1c7f
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-04-22 14:49:14 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,9 @@
 export function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1';
 
-  return fetch(`${BASE_URL}/name/${name}`).then(response => {
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
